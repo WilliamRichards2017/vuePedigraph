0570350f1a78b953e76c952abcde24ad5a1763b1
Will Richards <Trill@Wills-MacBook-Pro-2.local>
2019-03-28 15:44:40 -0600

    building out ped model, currently printing out imediate parents of node hovered over

---
+++ client/js/pedLine.js
@@ -0,0 +1,49 @@
+export default class pedLine{
+  constructor(line){
+    this.line = line;
+    this.familyID = -1;
+    this.individualID = -1;
+    this.maternalID = -1;
+    this.paternalID = -1;
+    this.sexID = -1;
+    this.sex = 'unknown';
+    this.phenotype = null;
+    this.genotype = null;
+
+    this.isRoot = false;
+    this.isLeaf = false;
+    this.parseSex();
+    this.populateLine();
+  }
+
+  populateLine(){
+
+    let self = this;
+
+    let cols = self.line.split(/(\s+)/).filter(function (e) {
+      return e.trim().length > 0;
+    });
+
+    self.familyID = parseInt(cols[0]);
+    self.individualID = parseInt(cols[1]);
+    self.paternalID = parseInt(cols[2]);
+    self.maternalID = parseInt(cols[3]);
+    self.sexID = parseInt(cols[4]);
+
+    if(self.maternalID === 0 && self.paternalID === 0){
+      self.isRoot = true;
+    }
+  }
+
+  parseSex(){
+    let self = this;
+    if(self.sexID === 1){
+      self.sex = 'male';
+    }
+    else if (this.sexID === 2){
+      self.sex = 'female';
+    }
+  }
+}
+
+
